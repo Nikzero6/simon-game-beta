72c347db5d5eba6f1cdb9c0d0d234b5685475566
Nikzero6 <nikzerosub@gmail.com>
2021-06-18 19:09:27 +0530

    final commit

--- index.js
+++ index.js
@@ -20,13 +20,11 @@ $(".game-btns").click(function() {
   var clickedBtnIndex = $(".game-btns").index(this);
   btnSound(clickedBtnIndex);
 
-  if(player == "computer") {
-    computerPress.push(clickedBtnIndex);
-  }
-  else if(player == "human") {
+  if(player == "human") {
     humanPress.push(clickedBtnIndex);
-    checkClick(computerPress, humanPress, level);  //checkClick
-    }
+    console.log(humanPress);
+    checkClick(computerPress, humanPress);  //checkClick
+  }
 });
 
 
@@ -44,11 +42,20 @@ $(document).keypress(function() {
   game(level);
 });
 
+//mobile START
+$(".clickMe").click(function() {
+  game(level);
+});
+
 //game function
 function game(level) {
 
-  //title change
-  $(".big-heading").text("Level "+level);
+  setTimeout(function(){
+    //title change
+    $(".big-heading").text("Level "+level);
+    //mobile title change
+    $(".mob-heading").text("Level "+level);
+  }, 500);
 
   //random game
   var randomNum = 0;
@@ -61,7 +68,7 @@ function game(level) {
     randomNum = Math.floor(Math.random()*4); //0-3
     btnPlay(randomNum);
     computerPress.push(randomNum);
-    console.log(computerPress[i-1]);
+    console.log(computerPress);
     i++;
 
     if (i>level) clearInterval(handle);
@@ -84,7 +91,7 @@ function btnPlay(num) {
 }
 
 //check Click
-function checkClick(computerPress, humanPress, num) {
+function checkClick(computerPress, humanPress) {
 
   if(humanPress.length < computerPress.length) {
     for(var i=0; i<humanPress.length; i++) {
@@ -93,23 +100,28 @@ function checkClick(computerPress, humanPress, num) {
       }
     }
   }
+  else if (humanPress[humanPress.length-1]==computerPress[computerPress.length-1]){
+    level++;
+    setTimeout(function() {
+    game(level);
+    }, 1000);
+  }
   else {
-    if(computerPress[num-1] != humanPress[num-1]) {
-        gameOver(level);
-    }
-    else {
-      level++;
-      setTimeout(function() {
-        game(level);
-      }, 1000);
-    }
+    gameOver(level);
   }
 }
 
 //gameOver function
 function gameOver(num) {
+  var gameOv = document.getElementById("gameOv");
+  gameOv.play();
   $(".main-container").css("background-color", "red");
   $(".big-heading").html("GAMEOVER!<br>SCORE: "+num*100);
+  $(".big-heading").css("color", "yellow");
+
+  //mobile gameOver
+  $(".mob-heading").html("GAMEOVER!<br>SCORE: "+num*100);
+  $(".mob-heading").css("color", "yellow");
 
   setTimeout(function() {
     location.reload();
